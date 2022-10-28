c49fe5bb305800f28b74fe910a4f48b353919526
VigneshNallasivam <vickyshanestark@gmail.com>
2022-10-28 14:50:04 +0530

    Added: UC-1 FirstName

---
+++ src/regex/FirstName.java
@@ -0,0 +1,20 @@
+package regex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class FirstName 
+{
+	
+static boolean isValid(String regex, String userInput)
+{
+return Pattern.compile(regex).matcher(userInput).matches();
+}
+	
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String firstNamePattern ="^[A-Z]{1}$";
+System.out.println("ENTER FIRST NAME");
+String firstName=sc.next();
+System.out.println(isValid(firstNamePattern, firstName));
+}
+}
