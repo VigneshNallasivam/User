c708cf525b2f2b2844fac0114019d92bb15a59b6
VigneshNallasivam <vickyshanestark@gmail.com>
2022-10-28 14:57:08 +0530

    Added: UC-2 LastName

---
+++ src/regex/LastName.java
@@ -0,0 +1,18 @@
+package regex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class LastName 
+{
+	static boolean isValid(String regex, String userInput)
+	{
+	return Pattern.compile(regex).matcher(userInput).matches();
+	}
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	String lastNamePattern ="^[A-Z]{1}[a-z]{2,15}$";
+	System.out.println("ENTER LAST NAME");
+	String lastName=sc.next();
+	System.out.println(isValid(lastNamePattern, lastName));
+	}
+}
