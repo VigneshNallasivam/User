bc9df13dce99836983f7bb630f63cf2db26ed20c
VigneshNallasivam <vickyshanestark@gmail.com>
2022-10-29 10:01:42 +0530

    Added:UC-8 Password4Special

---
+++ src/regex/Password4Special.java
@@ -0,0 +1,18 @@
+package regex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class Password4Special 
+{
+	static boolean isValid(String regex, String userInput)
+	{
+	return Pattern.compile(regex).matcher(userInput).matches();
+	}
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	String pass4Pattern ="^[0-9]{10}[@#%$]$";
+	System.out.println("ENTER PASSWORD OF TYPE-4 SPECIAL CHARACTER");
+	String pass4=sc.next();
+	System.out.println(isValid(pass4Pattern, pass4));
+	}
+}
